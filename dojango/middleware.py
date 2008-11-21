d5e9cdee852191246260357b6bdc898e368cd2a6
Tobias von Klipstein <klipstein@uxebu.com>
2008-11-21 23:54:18 +0000

    using the general function request.is_ajax() (since django 1.0)

--- dojango/middleware.py
+++ dojango/middleware.py
@@ -18,7 +18,7 @@ class AJAXSimpleExceptionResponse:
     """
     def process_exception(self, request, exception):
         #if settings.DEBUG:
-        if request.META.get('HTTP_X_REQUESTED_WITH', None) == 'XMLHttpRequest':
+        if request.is_ajax(): # new in django version 1.0
             import sys, traceback
             (exc_type, exc_info, tb) = sys.exc_info()
             response = "%s\n" % exc_type.__name__
