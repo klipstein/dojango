9b86d913d5aec93729f12164debfebfa0ad29b09
Tobias von Klipstein <klipstein@uxebu.com>
2008-11-23 15:22:24 +0000

    supporting django 0.96 (force_unicode was added in 1.0)

--- dojango/util/__init__.py
+++ dojango/util/__init__.py
@@ -1,6 +1,6 @@
 import os
+import datetime
 from decimal import Decimal
-from datetime import datetime
 
 from dojango.conf import settings # using the app-specific settings
 from django.core.serializers.json import DateTimeAwareJSONEncoder
@@ -9,8 +9,60 @@ from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.utils import simplejson as json
-from django.utils.encoding import force_unicode
 from django.utils.functional import Promise
+
+try:
+    # this is just available since django version 1.0
+    # google appengine does not provide this function yet!
+    from django.utils.encoding import force_unicode
+except Exception:
+    import types
+    # code copied from django version 1.0
+    class DjangoUnicodeDecodeError(UnicodeDecodeError):
+        def __init__(self, obj, *args):
+            self.obj = obj
+            UnicodeDecodeError.__init__(self, *args)
+
+        def __str__(self):
+            original = UnicodeDecodeError.__str__(self)
+            return '%s. You passed in %r (%s)' % (original, self.obj,
+                    type(self.obj))
+    
+    def force_unicode(s, encoding='utf-8', strings_only=False, errors='strict'):
+        """
+        Similar to smart_unicode, except that lazy instances are resolved to
+        strings, rather than kept as lazy objects.
+
+        If strings_only is True, don't convert (some) non-string-like objects.
+        """
+        if strings_only and isinstance(s, (types.NoneType, int, long, datetime.datetime, datetime.date, datetime.time, float)):
+            return s
+        try:
+            if not isinstance(s, basestring,):
+                if hasattr(s, '__unicode__'):
+                    s = unicode(s)
+                else:
+                    try:
+                        s = unicode(str(s), encoding, errors)
+                    except UnicodeEncodeError:
+                        if not isinstance(s, Exception):
+                            raise
+                        # If we get to here, the caller has passed in an Exception
+                        # subclass populated with non-ASCII data without special
+                        # handling to display as a string. We need to handle this
+                        # without raising a further exception. We do an
+                        # approximation to what the Exception's standard str()
+                        # output should be.
+                        s = ' '.join([force_unicode(arg, encoding, strings_only,
+                                errors) for arg in s])
+            elif not isinstance(s, unicode):
+                # Note: We use .decode() here, instead of unicode(s, encoding,
+                # errors), so that if s is a SafeString, it ends up being a
+                # SafeUnicode at the end.
+                s = s.decode(encoding, errors)
+        except UnicodeDecodeError, e:
+            raise DjangoUnicodeDecodeError(s, *e.args)
+        return s
 
 def debug(request):
     "Returns context variables helpful for debugging."
@@ -51,7 +103,7 @@ def json_encode(data):
         # see http://code.djangoproject.com/ticket/5868
         elif isinstance(data, Promise):
             ret = force_unicode(data)
-        elif isinstance(data, datetime):
+        elif isinstance(data, datetime.datetime):
             # For dojo.date.stamp we convert the dates to use 'T' as separator instead of space
             # i.e. 2008-01-01T10:10:10 instead of 2008-01-01 10:10:10
             ret = str(data).replace(' ', 'T')
@@ -118,7 +170,7 @@ def json_response(data, func_name=None, use_iframe=False):
     # The following are for IE especially
     ret['Pragma'] = "no-cache"
     ret['Cache-Control'] = "must-revalidate"
-    ret['If-Modified-Since'] = str(datetime.now())
+    ret['If-Modified-Since'] = str(datetime.datetime.now())
     return ret
 
 def to_dojo_data(items, identifier='id', num_rows=None):
