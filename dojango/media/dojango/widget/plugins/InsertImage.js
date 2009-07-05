f72935b2a38eec76488d19f8e84f55c127e37291
Tobias von Klipstein <klipstein@uxebu.com>
2009-07-05 12:34:05 +0000

    defined configuration options for the thumbnailpicker added some more documentation

--- dojango/media/dojango/widget/plugins/InsertImage.js
+++ dojango/media/dojango/widget/plugins/InsertImage.js
@@ -2,6 +2,15 @@ dojo.provide("dojango.widget.plugins.InsertImage");
 
 dojo.require("dijit._editor.plugins.LinkDialog");
 dojo.require("dojango.widget.ThumbnailPicker");
+
+dojango.widget.plugins.InsertImageConfig = {
+	// these values just can be overwritten, when the plugin is loaded synchronous
+	size:400,
+	thumbHeight:75,
+	thumbWidth:100,
+	isHorizontal:true
+}
+
 dojo.declare("dojango.widget.plugins.InsertImage",
 	dijit._editor.plugins.LinkDialog,
 	{
@@ -9,6 +18,8 @@ dojo.declare("dojango.widget.plugins.InsertImage",
 		//	An editor plugin that uses dojox.image.ThumbailPicker to select an image and inserting it into the editor.
 		//	Populates the ThumbnailPicker as the editor attribute "thumbnailPicker", where you can attach your store
 		//	via setDataStore. For store examples look at the dojox.image.ThumbnailPicker tests.
+		//	The InsertImage plugin simply extends the LinkDialog plugin which already contains the functionality
+		//	for inserting an image.
 		//
 		// example:
 		// |	// these css files are required:
@@ -31,32 +42,42 @@ dojo.declare("dojango.widget.plugins.InsertImage",
 		//size, thumbHeight, thumbWidth, isHorizontal <= setting these additional parameters
 		command: "insertImage",
 		linkDialogTemplate: [
-		            '<div id="${id}_thumbPicker" class="thumbPicker" dojoType="dojango.widget.ThumbnailPicker" size="400" isClickable="true"></div>',
-		            '<label for="${id}_textInput">${text}</label><input dojoType="dijit.form.ValidationTextBox" required="true" name="textInput" id="${id}_textInput"/>',
-		            '<div><button dojoType=dijit.form.Button type="submit">${set}</button></div>'
-		        ].join(""),
+					'<div id="${id}_thumbPicker" class="thumbPicker" dojoType="dojango.widget.ThumbnailPicker" size="${size}"',
+					'thumbHeight="${thumbHeight}" thumbWidth="${thumbWidth}" isHorizontal="${isHorizontal}" isClickable="true"></div>',
+					'<label for="${id}_textInput">${text}</label><input dojoType="dijit.form.ValidationTextBox" required="true" name="textInput" id="${id}_textInput"/>',
+					'<div><button dojoType=dijit.form.Button type="submit">${set}</button></div>'
+				].join(""),
 		_picker: null,
 		_textInput: null,
+		_currentItem: null,
 		_initButton: function(){
+			this.linkDialogTemplate = dojo.string.substitute(this.linkDialogTemplate,
+				dojango.widget.plugins.InsertImageConfig, 
+				function(value, key){
+					return value ? value : "${" + key + "}"; // we keep the non-matching keys
+				}
+			);
 			this.inherited(arguments);
-			this._picker = dijit.byNode(dojo.query("[widgetId]", this.dropDown.domNode)[0]); // creating a unique id should happen outside of initButton
+			// creating a unique id should happen outside of _initButton (see LinkDialog), so accessing 
+			// the widgets in the linkDialog would be easier!
+			this._picker = dijit.byNode(dojo.query("[widgetId]", this.dropDown.domNode)[0]);
 			this._textInput = dijit.byNode(dojo.query("[widgetId]", this.dropDown.domNode)[1]);
 			
 			dojo.subscribe(this._picker.getClickTopicName(), dojo.hitch(this, "_markSelected"));
 			this.dropDown.execute = dojo.hitch(this, "_customSetValue");
-			var oldOpen = this.dropDown.onOpen;
 			var _this=this;
 			this.dropDown.onOpen = function(){
 				_this._onOpenDialog();
 				dijit.TooltipDialog.prototype.onOpen.apply(this, arguments);
-				// resetting scroller (onOpen it is set to 0!)
-				var a = _this._picker._thumbs[_this._picker._thumbIndex],
-					b = _this._picker.thumbsNode;
+				// resetting scroller (onOpen scrollLeft set to 0!)
+				var p = _this._picker, a = p._thumbs[p._thumbIndex],
+					b = p.thumbsNode;
 				if(typeof(a) != "undefined" && typeof(b) != "undefined" ){
-					var left = a.offsetLeft - b.offsetLeft;
-					_this._picker.thumbScroller.scrollLeft = left;
+					var left = a[p._offsetAttr] - b[p._offsetAttr];
+					p.thumbScroller[p._scrollAttr] = left;
 				}
 			}
+			// the popup needs to be generated, so the ThumbnailPicker can align the images!
 			dijit.popup.prepare(this.dropDown.domNode);
 			// assigning the picker to the editor
 			this.editor.thumbnailPicker = this._picker;
@@ -66,20 +87,15 @@ dojo.declare("dojango.widget.plugins.InsertImage",
 			if(! this._currentItem) {
 				return false;
 			}
+			// pass the url of the current selected image to the setValue method
 			args.urlInput = this._currentItem['largeUrl'] ? this._currentItem['largeUrl'] : this._currentItem['url'];
 			this.setValue(args);
 		},
 		
-		_currentItem: null,
-		_markSelected: function(item, idx){
+		_markSelected: function(item){
 			// url, largeUrl, title, link
 			this._currentItem = item;
-			if(item.title){
-				this._textInput.attr("value", item.title);
-			}
-			else {
-				this._textInput.attr("value", "");
-			}
+			this._textInput.attr("value", item.title ? item.title : "");
 			this._picker.reset();
 			dojo.addClass(this._picker._thumbs[item.index], "imgSelected");
 		}
