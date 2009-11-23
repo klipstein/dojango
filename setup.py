61ca7e54c35b978aaf59cf962ebb36ce6d16e8c3
Tobias von Klipstein <klipstein@uxebu.com>
2009-11-23 21:11:39 +0000

    changed setup.py (added some more information)

--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name = "dojango",
-    version = "svn",
+    version = "0.4.5",
     url = 'http://code.google.com/p/dojango/',
     license = 'License :: OSI Approved :: BSD License',
-    description = "Reusable django application that helps you to use the client-side framework dojo",
-    author = 'Tobias Klipstein',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    description = 'Reusable django application that helps you to use the client-side framework dojo',
+    keywords = 'dojo,django,dojango,javascript',
+    author = 'Tobias von Klipstein',
+    author_email='tk@uxebu.com',
     packages = find_packages('.'),
     package_dir = {'': '.'},
     install_requires = ['setuptools'],
     package_data={'': ['*.html']},
-
+    zip_safe=False,
 )
-
