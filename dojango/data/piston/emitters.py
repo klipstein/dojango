e46b1ad29dffd77911dae53ea71821058c381544
Fabian Topfstedt <fabian.topfstedt@gmail.com>
2010-08-31 20:30:59 +0000

    reduced db queries, changed docstrings

--- dojango/data/piston/emitters.py
+++ dojango/data/piston/emitters.py
@@ -6,18 +6,28 @@ from piston.validate_jsonp import is_valid_jsonp_callback_value
 
 
 class DojoDataEmitter(Emitter):
+    """
+    This emitter is designed to render dojo.data.ItemFileReadStore compatible
+    data.
+
+    Requires your handler to expose the `id` field of your model, that Piston
+    excludes in the default setting. The item's label is the unicode
+    representation of your model unless it already has a field with the
+    name `_unicode`.
+
+    Optional GET variables:
+        `callback`: JSONP callback
+        `indent`: Number of spaces for JSON indentation
+
+    If you serialize Django models and nest related models (which is a common
+    case), make sure to set the `hierarchical` parameter of the
+    ItemFileReadStore to false (which defaults to true).
+    """
 
     def render(self, request):
         """
         Renders dojo.data compatible JSON if self.data is a QuerySet, falls
-        back to standard JSON. Requires your handler to expose the `id`
-        field of your model, that Piston excludes per default. The item's
-        label is the unicode representation of your model unless it already
-        has a field with the name `_unicode`.
-
-        Optional GET variables:
-            `callback`: JSONP callback
-            `indent`: Number of spaces for JSON indentation
+        back to standard JSON.
         """
         callback = request.GET.get('callback', None)
         try:
@@ -29,8 +39,9 @@ class DojoDataEmitter(Emitter):
 
         if isinstance(self.data, QuerySet):
             unicode_lookup_table = dict()
+
             [unicode_lookup_table.__setitem__(item.pk, unicode(item)) \
-                for item in self.data.all()]
+                for item in self.data]
 
             for dict_item in data:
                 try:
@@ -45,13 +56,12 @@ class DojoDataEmitter(Emitter):
                 'identifier': 'id',
                 'items': data,
                 'label': '_unicode',
-                'numRows': len(data),
+                'numRows': self.data.count(),
             }
 
         serialized_data = json.dumps(data, ensure_ascii=False,
             cls=DateTimeAwareJSONEncoder, indent=indent)
 
-        # Callback
         if callback and is_valid_jsonp_callback_value(callback):
             return '%s(%s)' % (callback, serialized_data)
 
@@ -59,5 +69,8 @@ class DojoDataEmitter(Emitter):
 
 
 def register_emitters():
+    """
+    Registers the DojoDataEmitter with the name 'dojodata'.
+    """
     Emitter.register('dojodata', DojoDataEmitter,
         'application/json; charset=utf-8')
