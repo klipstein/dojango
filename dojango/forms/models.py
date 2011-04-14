0310b796f6a4d6d1b72aead3091bd01e80709c4c
Tobias von Klipstein <klipstein@uxebu.com>
2011-04-14 16:58:36 +0000

    fixes #75 and #79. Will break if form_for_fields is used from dojango somewhere. Alternatively this could be imported directly from django.forms.models, if that is used within older django versions.

--- dojango/forms/models.py
+++ dojango/forms/models.py
@@ -15,8 +15,7 @@ from dojango.forms.widgets import DojoWidgetMixin, Textarea, Select, SelectMulti
 
 __all__ = (
     'ModelForm', 'BaseModelForm', 'model_to_dict', 'fields_for_model',
-    'save_instance', 'form_for_fields', 'ModelChoiceField',
-    'ModelMultipleChoiceField',
+    'save_instance', 'ModelChoiceField', 'ModelMultipleChoiceField',
 )
     
 class ModelChoiceField(DojoFieldMixin, models.ModelChoiceField):
