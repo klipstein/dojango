10e7e56431823e309e96b9d7d5b5e7ef1084b91b
Roger Hunwicks <roger@tonic-solutions.com>
2013-09-16 10:09:18 +0600

    Use force_str to allow import when using unicode_literals - fixes #112

--- dojango/util/media.py
+++ dojango/util/media.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from dojango.conf import settings as dojango_settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils._os import safe_join
+from django.utils.encoding import force_str
 if django_version >= (1, 5, 0):
     from django.conf.urls import patterns
 else:
@@ -21,7 +22,7 @@ def find_app_dir(app_name):
         if a is None:
             mod = __import__(m, {}, {}, [])
         else:
-            mod = getattr(__import__(m, {}, {}, [a]), a)
+            mod = getattr(__import__(m, {}, {}, [force_str(a)]), a)
         return path.dirname(path.abspath(mod.__file__))
     except ImportError, e:
         raise ImproperlyConfigured, 'ImportError %s: %s' % (app_name, e.args[0])
