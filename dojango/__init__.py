fbd6ea5170c4364213ab3fe69d0ffa17a4c4b47d
Tobias von Klipstein <klipstein@uxebu.com>
2014-01-04 13:14:44 +0100

    Cleanup setup.py

--- dojango/__init__.py
+++ dojango/__init__.py
@@ -1,16 +1,2 @@
-VERSION = (0, 5, 7, 'alpha', 0)
-
-def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
-    #from django.utils.version import get_svn_revision
-    #svn_rev = get_svn_revision()
-    #if svn_rev != u'SVN-unknown':
-    #    version = "%s %s" % (version, svn_rev)
-    return version
+# following PEP 386
+__version__ = "0.5.7-alpha"
--- setup.py
+++ setup.py
@@ -1,73 +1,128 @@
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from __future__ import print_function
 import os
+import re
 import sys
+import codecs
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+from setuptools import setup, find_packages
 
-# COPIED FROM SETUP.PY OF DJANGO
-def fullsplit(path, result=None):
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+# Provided as an attribute, so you can append to these instead
+# of replicating them:
+standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
+                                './dist', 'EGG-INFO', '*.egg-info')
+
+
+# (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
+# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+# Note: you may want to copy this into your setup.py file verbatim, as
+# you can't import this from another package, when you don't know if
+# that package is installed yet.
+def find_package_data(where='.', package='',
+                      exclude=standard_exclude,
+                      exclude_directories=standard_exclude_directories,
+                      only_in_packages=True,
+                      show_ignored=False):
     """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
+Return a dictionary suitable for use in ``package_data``
+in a distutils ``setup.py`` file.
 
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
+The dictionary looks like::
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-dojango_dir = 'dojango'
+{'package': [files]}
 
-for dirpath, dirnames, filenames in os.walk(dojango_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+Where ``files`` is a list of all the files in that package that
+don't match anything in ``exclude``.
 
-# Small hack for working with bdist_wininst.
-# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
-if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
-    for file_info in data_files:
-        file_info[0] = '\\PURELIB\\%s' % file_info[0]
+If ``only_in_packages`` is true, then top-level directories that
+are not packages won't be included (but directories under packages
+will).
 
-# Dynamically calculate the version based on dojango.VERSION.
-version = __import__('dojango').get_version()
-if u'SVN' in version:
-    version = ' '.join(version.split(' ')[:-1])
+Directories matching any pattern in ``exclude_directories`` will
+be ignored; by default directories with leading ``.``, ``CVS``,
+and ``_darcs`` will be ignored.
 
+If ``show_ignored`` is true, then all the files that aren't
+included in package data are shown on stderr (for debugging
+purposes).
+
+Note patterns use wildcards, or can be exact paths (including
+leading ``./``), and all searching is case-insensitive.
+"""
+
+    out = {}
+    stack = [(convert_path(where), '', package, only_in_packages)]
+    while stack:
+        where, prefix, package, only_in_packages = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print("Directory %s ignored by pattern %s" %
+                                  (fn, pattern), file=sys.stderr)
+                        break
+                if bad_name:
+                    continue
+                if (os.path.isfile(os.path.join(fn, '__init__.py')) and not prefix):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                    stack.append((fn, '', new_package, False))
+                else:
+                    stack.append((fn, prefix + name + '/', package, only_in_packages))
+            elif package or not only_in_packages:
+                # is a file
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print("File %s ignored by pattern %s" %
+                                  (fn, pattern), file=sys.stderr)
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix + name)
+    return out
 
 setup(
-    name = "dojango",
-    version = version,
+    name="dojango",
+    version=find_version("dojango", "__init__.py"),
     url = 'http://code.google.com/p/dojango/',
-    license = 'License :: OSI Approved :: BSD License',
+    license='License :: OSI Approved :: MIT License',
+    description = 'Reusable django application that helps you to use the client-side framework dojo',
+    keywords = 'dojo,django,dojango,javascript',
+    author = 'Tobias von Klipstein',
+    author_email='tk@uxebu.com',
+    packages=find_packages(),
+    package_data=find_package_data(),
     classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",
         "Environment :: Web Environment",
     ],
-    description = 'Reusable django application that helps you to use the client-side framework dojo',
-    keywords = 'dojo,django,dojango,javascript',
-    author = 'Tobias von Klipstein',
-    author_email='tk@uxebu.com',
-    packages = packages,
-    data_files = data_files,
+    zip_safe=False,
 )
